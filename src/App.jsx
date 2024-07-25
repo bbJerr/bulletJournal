6110757670c0268659b17b85149f0560ca00ae9a
bbJerr <jerrakirstenr22@gmail.com>
2024-07-25 22:14:55 +0800

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import JournalPage from "./components/JournalPage";
 import { LandingPage } from "./components/LandingPage";
+import "./App.css";
 
 const App = () => {
   return (
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./landingPage.css";
 
 export const LandingPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Welcome to the Journal App</h1>
-      <button onClick={() => navigate("/home")}>Go to Calendar</button>
+    <div className="grid-background">
+      <div className="landing-page-content">
+        <img src="/bujo-logo.png" alt="bujo logo" />
+        <button onClick={() => navigate("/home")} className="btn"><span>start journal entry</span></button>
+      </div>
     </div>
   );
 };
